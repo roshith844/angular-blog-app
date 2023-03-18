ea6f13293fd7c75f13b83463da3cb884e77b0744
ROSHITH P <roshith844@gmail.com>
2023-03-18 11:42:25 +0530

    fix: determine roles when app loads

--- blog-app/src/app/app.component.ts
+++ blog-app/src/app/app.component.ts
@@ -16,38 +16,26 @@ export class AppComponent implements OnInit {
   ) { }
   ngOnInit(): void {
     this.userDetailsService.getUserDetails().subscribe((response: any) => {
-
-      if (response.response.loggedIn == true) {
+console.log(response)
+      if (response.loggedIn == true) {
 
         this.userDetailsService.modifyNameOfUser(response.name)
 
         if (response.role == 'user') {
           this.userLoginService.markAsLoggedIn()
-        } else {
-          this.userLoginService.markAsLoggedOut()
-        }
-
-        if (response.role == 'writer') {
-          this.writerService.markAsWriter()
-        } else {
           this.writerService.markAsNotAWriter()
-        }
-
-        if (response.role === 'admin') {
+        } else if (response.role == 'writer') {
+          this.userLoginService.markAsLoggedIn()
+          this.writerService.markAsWriter()
+        } else if (response.role === 'admin') {
           this.adminLoginService.markAsLoggedIn()
         } else {
+          this.userLoginService.markAsLoggedOut()
+          this.writerService.markAsNotAWriter()
           this.adminLoginService.markAsLoggedOut()
         }
-
       }
 
-
-
-
-
-
     })
-
-
   }
 }
--- blog-app/src/app/services/writer/writer.service.ts
+++ blog-app/src/app/services/writer/writer.service.ts
@@ -4,7 +4,7 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class WriterService {
-  public isWriter: boolean = false
+  public isWriter!: boolean ;
   markAsWriter() {
     this.isWriter = true
   }
