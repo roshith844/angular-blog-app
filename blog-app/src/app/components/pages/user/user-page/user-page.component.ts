39eb9349e66bba02ba916b2e5774944d4a36a547
ROSHITH P <roshith844@gmail.com>
2023-03-23 21:06:58 +0530

    feat: fetch user details when user page loads

--- blog-app/src/app/components/pages/user/user-page/user-page.component.ts
+++ blog-app/src/app/components/pages/user/user-page/user-page.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { AdminLoginService } from 'src/app/services/admin/admin-login.service';
+import { UserDetailsService } from 'src/app/services/user-details.service';
+import { UserLoginService } from 'src/app/services/user/user-login.service';
+import { WriterService } from 'src/app/services/writer/writer.service';
 
 @Component({
   selector: 'app-user-page',
@@ -6,10 +10,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-page.component.css']
 })
 export class UserPageComponent implements OnInit {
-
+  constructor(public userDetailsService: UserDetailsService, public writerService: WriterService, public userLoginService: UserLoginService,
+    private adminLoginService: AdminLoginService
+  ) { }
 ngOnInit(): void {
   
-  
+  this.userDetailsService.getUserDetails().subscribe((response: any) => {
+    if (response.loggedIn == true) {
+
+      this.userDetailsService.modifyNameOfUser(response.name)
+      this.userDetailsService.modifyProfilePictureOfUser(response.image)
+      this.userDetailsService.modifyIdOfUser(response.userId)
+
+
+      if (response.role == 'user') {
+        this.userLoginService.markAsLoggedIn()
+        this.writerService.markAsNotAWriter()
+      } else if (response.role == 'writer') {
+        this.userLoginService.markAsLoggedIn()
+        this.writerService.markAsWriter()
+      } else if (response.role === 'admin') {
+        this.adminLoginService.markAsLoggedIn()
+      } else {
+        this.userLoginService.markAsLoggedOut()
+        this.writerService.markAsNotAWriter()
+        this.adminLoginService.markAsLoggedOut()
+      }
+    }
+
+  })
 }
 
 
